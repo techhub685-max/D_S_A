2558b7cef5bc5d1c705a037f15d258e1e51699ef
techhub685-max <techhub685@gmail.com>
2026-01-20 22:34:47 +0530

    Create OptimizedInsertionSort.java

---
+++ OptimizedInsertionSort.java
@@ -0,0 +1,23 @@
+class OptimizedInsertionSort {
+
+    static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {9, 5, 1, 4, 3};
+        insertionSort(arr);
+
+        for (int x : arr)
+            System.out.print(x + " ");
+    }
+}
